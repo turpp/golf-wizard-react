657540afee77899cc2a1f81ce0a7c0a233079692
turpp <white.tripp@icloud.com>
2021-05-27 03:02:09 -0500

    rough sketch of navbar

---
+++ src/generalComp/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Navbar(){
+
+    return(
+        <nav>
+            <div>
+                <a href='/'>Tournament Wizard</a>
+            </div>
+            <div>
+            <a href='/login'>Login</a>
+            <a href='/signup'>Sign Up</a>
+
+            </div>
+
+        </nav>
+
+        
+    )
+}
+
+export default Navbar
