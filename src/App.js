bb66ebb82db84f9f017f3c9bc837438578c04039
turpp <white.tripp@icloud.com>
2021-05-27 03:01:04 -0500

    removed boilpate code and added router and components

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
-import {connect} from 'react-redux'
 import React from 'react';
 import {BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
+import Home from './generalComp/Home'
+import Footer from './generalComp/Footer'
+import Navbar from './generalComp/Navbar'
+import Login from './generalComp/Login'
+import Signup from './generalComp/Signup'
+
 
 class App extends React.Component{
 
   render(){
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Router>
+          <Navbar/>
+          <Switch>
+            <Route exact path ='/' component={Home}/>
+            <Route exact path ='/login' component={Login}/>
+            <Route exact path ='/signup' component={Signup}/>
+          </Switch>
+      </Router>
+      <Footer/>
       </div>
     );
   }
